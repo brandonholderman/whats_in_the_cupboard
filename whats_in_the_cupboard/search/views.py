5ac622a855210f5121c76f57b56ceda28d5109e1
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-12-29 01:00:49 -0800

    created requirements, database, basic structure: issues with superuser creation

---
+++ whats_in_the_cupboard/search/views.py
@@ -0,0 +1,18 @@
+# from django.shortcuts import render
+# from django.views import TemplateView, ListView
+
+# Create your views here.
+# class HomeView(View):
+#     """
+#     Home view class.
+#     """
+#     # context_obj =
+#     template_name = 'generic/home.html'
+
+#     def get(self, request):
+#         context = {
+            
+#         }
+
+
+    
