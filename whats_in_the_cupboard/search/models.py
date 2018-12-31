10f0fb360f88d1d0f4c4a4ba6bf88f073330cba5
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-12-31 01:25:05 -0800

    added basic search model

--- whats_in_the_cupboard/search/models.py
+++ whats_in_the_cupboard/search/models.py
@@ -1,5 +1,26 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+# from django.dispatch import receiver
+# from rest_framework.authtoken.models import Token
 
-# # Create your models here.
-# class Search(models.Model):
-    
+
+# Create your models here.
+class Search(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='search')
+
+    def __str__(self):
+        """returns string representation to user"""
+        return self.user.username
+
+    @classmethod
+    def active(cls):
+        """Return all active profile instances"""
+        return cls.objects.filter(is_active=True)
+
+# @receiver(models.signals.post_save, sender=User)
+# def create_profile(sender, **kwargs):
+#     if kwargs['created']:
+#         Token.objects.create(user=kwargs['instance'])
+#         profile = ImagerProfile(user=kwargs['instance'])
+#         profile.save()
--- whats_in_the_cupboard/whats_in_the_cupboard/settings.py
+++ whats_in_the_cupboard/whats_in_the_cupboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whats_in_the_cupboard',
+    'search',
 ]
 
 MIDDLEWARE = [
