177bda4489b05ad1a96575ac0d310a36f2995e43
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-12-31 16:58:11 -0800

    created dev branch, started profile model: still issues connecting to db when creating superuser

--- whats_in_the_cupboard/user_profile/models.py
+++ whats_in_the_cupboard/user_profile/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.dispatch import receiver
 # from rest_framework.authtoken.models import Token
+# from django.dispatch import receiver
 
 
-# Create your models here.
-class Search(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='search')
 
--- whats_in_the_cupboard/whats_in_the_cupboard/settings.py
+++ whats_in_the_cupboard/whats_in_the_cupboard/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whats_in_the_cupboard',
-    'search',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
